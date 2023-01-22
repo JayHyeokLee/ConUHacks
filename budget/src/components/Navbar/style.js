e596a4dd3558569557270b9ca4a0c0101b361f55
JayHyeokLee <jay.lee98@hotmail.com>
2023-01-22 06:46:40 -0500

    Fixed Navbar alignment

--- budget/src/components/Navbar/style.js
+++ budget/src/components/Navbar/style.js
@@ -4,10 +4,9 @@ import { Link } from 'react-router-dom';
 export const StyledNavbar = styled.nav`
     display: flex;
     align-items: center;
-    justify-content: flex-end;
     width: 100%;
     height: 70px;
-    padding: 0 30px;
+    padding: 10px;
     color: #ffffff;
     background-color: #202023;
     box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
@@ -15,7 +14,9 @@ export const StyledNavbar = styled.nav`
 
 export const NavItemLink = styled(Link)`
     color: inherit;
-    margin-left: 16px;
+    align-items: center;
+    margin-left: 250px;
+    margin-right: 20px;
     ${props => props.fill && css`
         padding: 8px 16px;
         border-radius: 4px;
